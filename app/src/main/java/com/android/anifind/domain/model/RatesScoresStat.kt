39c151d2c6614f9d3a9514103d6cf551bef86e77
Александр Классен <aiklassen@edu.hse.ru>
2022-01-13 10:08:08 +0500

    Сокращение не используемых данных

--- app/src/main/java/com/android/anifind/domain/model/RatesScoresStat.kt
+++
@@ -1,6 +0,0 @@
-package com.android.anifind.domain.model
-
-data class RatesScoresStat (
-	val name : Int,
-	val value : Int
-)
--- app/src/main/java/com/android/anifind/domain/model/RatesStatusesStat.kt
+++
@@ -1,6 +0,0 @@
-package com.android.anifind.domain.model
-
-data class RatesStatusesStat (
-	val name : String,
-	val value : Int
-)
